4d5ea82b43f94961bd3253e1fb7eed2ded430785
Berkay Özdemir <berkay.ozdemir@optiyol.com>
2022-05-30 11:26:21 +0300

    keyboard bug fixed

--- lib/auth/SignupPage.dart
+++ lib/auth/SignupPage.dart
@@ -76,148 +76,143 @@ class _SignupPageState extends State<SignupPage> {
           ),
         ],
       ),
-      body: Align(
-        alignment: Alignment.bottomCenter,
-        child: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: SingleChildScrollView(
-            reverse: false,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(25),
-                  child: Image.asset(
-                    "assets/images/kitap.png",
-                    height: MediaQuery.of(context).size.height / 4,
-                  ),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(25),
+                child: Image.asset(
+                  "assets/images/kitap.png",
                 ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  decoration: const BoxDecoration(
-                    color: lightColor,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25)),
-                  ),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 20),
-                      const Text("Kayıt Ol",
-                          style: TextStyle(
-                              fontSize: 35, fontWeight: FontWeight.bold)),
-                      const SizedBox(height: 20),
-                      DengeInput(
-                        labelText: "İsim Soyisim",
-                        controller: _nameController,
-                        suffixIcon: Icons.person,
-                        validator: (value) {
-                          if (value!.length < 3) {
-                            return "İsim ve Soyisminiz 3 karakterden kısa olamaz";
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      DengeInput(
-                        labelText: "Email",
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        suffixIcon: Icons.email,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Email Giriniz";
-                          } else if (!EmailValidator.validate(value)) {
-                            return "Geçerli Bir Email Giriniz";
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      DengeInput(
-                        labelText: "Şifre",
-                        controller: _passwordController,
-                        obscureText: true,
-                        suffixIcon: Icons.lock,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Şifre Giriniz";
-                          } else if (value.length < 8) {
-                            return "Şifreniz 8 Karakterden az Olamaz";
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      DengeInput(
-                        labelText: "Şifre Tekrar",
-                        controller: _passwordAgainController,
-                        obscureText: true,
-                        suffixIcon: Icons.lock,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Şifre Giriniz";
-                          } else if (value.length < 8) {
-                            return "Şifreniz 8 Karakterden az Olamaz";
-                          } else if (value != _passwordController.text) {
-                            return "Şifreler Aynı Değil";
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: DengeButton(
-                            label: "Kayıt Ol",
-                            onPressed: () async {
-                              bool _validate =
-                                  _formKey.currentState!.validate();
-                              if (_validate) {
-                                await auth
-                                    .createUserWithEmailAndPassword(
-                                  email: _emailController.text,
-                                  password: _passwordController.text,
-                                )
-                                    .then((value) async {
-                                  Map<String, dynamic> userMap = {
-                                    "nameSurname": _nameController.text,
-                                  };
-                                  await firestore
-                                      .collection("users")
-                                      .doc(value.user!.uid)
-                                      .set(userMap);
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                decoration: const BoxDecoration(
+                  color: lightColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25)),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text("Kayıt Ol",
+                        style: TextStyle(
+                            fontSize: 35, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    DengeInput(
+                      labelText: "İsim Soyisim",
+                      controller: _nameController,
+                      suffixIcon: Icons.person,
+                      validator: (value) {
+                        if (value!.length < 3) {
+                          return "İsim ve Soyisminiz 3 karakterden kısa olamaz";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    DengeInput(
+                      labelText: "Email",
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      suffixIcon: Icons.email,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Email Giriniz";
+                        } else if (!EmailValidator.validate(value)) {
+                          return "Geçerli Bir Email Giriniz";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    DengeInput(
+                      labelText: "Şifre",
+                      controller: _passwordController,
+                      obscureText: true,
+                      suffixIcon: Icons.lock,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Şifre Giriniz";
+                        } else if (value.length < 8) {
+                          return "Şifreniz 8 Karakterden az Olamaz";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    DengeInput(
+                      labelText: "Şifre Tekrar",
+                      controller: _passwordAgainController,
+                      obscureText: true,
+                      suffixIcon: Icons.lock,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Şifre Giriniz";
+                        } else if (value.length < 8) {
+                          return "Şifreniz 8 Karakterden az Olamaz";
+                        } else if (value != _passwordController.text) {
+                          return "Şifreler Aynı Değil";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: DengeButton(
+                          label: "Kayıt Ol",
+                          onPressed: () async {
+                            bool _validate = _formKey.currentState!.validate();
+                            if (_validate) {
+                              await auth
+                                  .createUserWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              )
+                                  .then((value) async {
+                                Map<String, dynamic> userMap = {
+                                  "nameSurname": _nameController.text,
+                                };
+                                await firestore
+                                    .collection("users")
+                                    .doc(value.user!.uid)
+                                    .set(userMap);
 
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: ((context) =>
-                                              const DashboardScreen())));
-                                }).onError((error, stackTrace) {
-                                  final snackBar = SnackBar(
-                                    content: Text(
-                                      error.toString(),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    action: SnackBarAction(
-                                        textColor: darkColor,
-                                        label: "Kapat",
-                                        onPressed: () {}),
-                                  );
-                                  ScaffoldMessenger.of(context)
-                                      .showSnackBar(snackBar);
-                                });
-                              }
-                            },
-                          )),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: ((context) =>
+                                            const DashboardScreen())));
+                              }).onError((error, stackTrace) {
+                                final snackBar = SnackBar(
+                                  content: Text(
+                                    error.toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  action: SnackBarAction(
+                                      textColor: darkColor,
+                                      label: "Kapat",
+                                      onPressed: () {}),
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              });
+                            }
+                          },
+                        )),
+                    const SizedBox(height: 20),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
